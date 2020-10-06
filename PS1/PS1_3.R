ae17d16d599103c83d50b1d1f339142e228c1c4d
ViolentElder <wwh1059042729@163.com>
2020-10-07 01:27:48 +0800

    更新代码

--- PS1/PS1_3.R
+++ PS1/PS1_3.R
@@ -3,9 +3,12 @@
 Pascal_triangle <- function(n){
   Pascal <- c()
   for (i in 0:n) {
-    temp <- factorial(n - 1)/(factorial(i - 1) * factorial(n - i))
+    if(i == 0)
+      temp<- 1
+    else
+      temp <- factorial(n - 1)/(factorial(i - 1) * factorial(n - i))
     Pascal[i] <- temp
   }
   return(Pascal)
 }
-Pascal_triangle(10)
+Pascal_triangle(5)
